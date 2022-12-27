a505a652a3a0a2fccedc5029725575db5fbe825e
Harun29 <117188375+Harun29@users.noreply.github.com>
2022-12-27 13:01:35 +0100

    installed fontAwsome and made the foundation for navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,18 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Navbar = () => {
   return ( 
-    pass
+    <nav>
+      <div className="name">
+        <h2>Eldar K.</h2>
+      </div>
+
+      <div className="menu-dropdown">
+        <FontAwesomeIcon icon={faBars} size="xl" />
+      </div>
+
+    </nav>
    );
 }
  
