af57ebcc244dba44de33a4109585c4c3c9690a94
Harun29 <117188375+Harun29@users.noreply.github.com>
2022-12-27 16:39:59 +0100

    made the dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,54 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
   return ( 
-    <nav>
+  <header>
+    {!dropdown ? (<nav>
       <div className="name">
         <h2>Eldar K.</h2>
       </div>
 
       <div className="menu-dropdown">
-        <FontAwesomeIcon icon={faBars} size="xl" />
-      </div>
+        <button onClick={handleDropdown}><FontAwesomeIcon icon={faBars} size="xl" /></button>
+      </div> 
+    </nav>) : null}
 
-    </nav>
-   );
+    {dropdown ? (
+      <nav className="dropdown">
+        <div className="navigation">
+          <button onClick={handleDropdown}>
+            <FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon>
+          </button>
+        </div>
+
+        <div className="navigation">
+          <button>Home</button>
+        </div>
+
+        <div className="navigation">
+          <button>Contact</button>
+        </div>
+
+        <div className="navigation">
+          <button>About me</button>
+        </div>
+
+        <div className="navigation">
+          <button>Recepies</button>
+        </div>
+      </nav>
+    ) : null}
+  </header>
+  );
 }
  
 export default Navbar;
