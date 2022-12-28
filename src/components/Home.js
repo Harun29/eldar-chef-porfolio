f02a204bf3c2fe32d89f73557a5175893e83d885
Harun29 <117188375+Harun29@users.noreply.github.com>
2022-12-28 23:59:53 +0100

    I am mentally disabled: I cant make my background blured

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,6 @@ const Home = () => {
   return (  
     <main>
       <div className="welcome">
-      {/* <img 
-          src="https://w7.pngwing.com/pngs/356/205/png-transparent-chef-cooking-chef-food-hand-cook.png" 
-          alt="" 
-        /> */}
         <div className="intro">
           <h1>Hello,</h1>
           <h2>I'm Eldar Karahmetovic</h2>
