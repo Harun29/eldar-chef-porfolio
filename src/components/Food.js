2f1ae622b461c1af14dbff5e8b97a31fc216c32e
harun <ibrahimovic.harun01@gmail.com>
2022-12-31 15:46:10 +0100

    not so fancy page scroll on button

--- src/components/Food.js
+++ src/components/Food.js
@@ -5,10 +5,12 @@ import { useRef } from "react";
 
 const Food = () => {
   
-  const ref = useRef();
+  const currentPage = useRef();
+  const nextPage = useRef();
   
   const handleClick = () => {
-    ref.current?.scrollIntoView({behaviour: 'smooth'})
+    currentPage.current?.scrollIntoView({behaviour: 'smooth'});
+    currentPage.current = nextPage.current;
   }
 
   return (  
@@ -19,35 +21,34 @@ const Food = () => {
           </div>
           <div className="food-pictures">
             
-              <Link>
+              <Link to="/">
                 <div className="food">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7g2INAXqxmY-PUEwX9B9r-kEt44N0z0VzLh6g-GYDPg&s" alt="" />
                 </div>
               </Link>
-              <Link>
+              <Link to="/">
                 <div className="food">
                     <img src="https://food-guide.canada.ca/sites/default/files/styles/square_400_x_400/public/2020-12/CFGPlate-crop400x400.jpg" alt="" />
                 </div>
               </Link>
 
-              <Link>
-                <div className="food" ref={ref}>
+              <Link to="/">
+                <div className="food">
                   <img src="https://www.diabetesfoodhub.org/system/user_files/Images/lunch-dinner-chicken-collard-greens-sweet-potato.jpg" alt="" />
                 </div>
               </Link>
 
-              <Link>
+              <Link to="/" ref={currentPage}>
                 <div className="food">
                   <img src="https://media.istockphoto.com/id/168340083/photo/choosemyplate-healthy-food-and-plate-of-usda-balanced-diet-recommendation.jpg?s=612x612&w=0&k=20&c=S5OYuPg-n1fuOOgReQf9xPuFgA1i-UO54Uj2u1RD7iU=" alt="" />
                 </div>
               </Link>
             
-            
-            <Link>
-              <div className="food">
-                <img src="https://images.unsplash.com/photo-1539735257177-0d3949225f96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhdGUlMjBvZiUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80" alt="" />
-              </div>
-            </Link>
+              <Link to="/" ref={nextPage}>
+                <div className="food">
+                  <img src="https://images.unsplash.com/photo-1539735257177-0d3949225f96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxhdGUlMjBvZiUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80" alt="" />
+                </div>
+              </Link>
 
           </div>
           <button onClick={handleClick} className="food-button">
@@ -55,7 +56,7 @@ const Food = () => {
           </button>
 
           <div className="recepies-button">
-            <button><Link>Get my recepies</Link></button>
+            <button><Link to="/">Get my recepies</Link></button>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
           <h2>I'm <span>Chef</span></h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tortor in augue placerat luctus. Cras placerat, nisi in pulvinar imperdiet, ligula est vestibulum dui, vel semper velit mauris bibendum dui.</p>
           <div className="first-button">
-            <button><Link>My resume</Link></button>
+            <button><Link to="/">My resume</Link></button>
           </div>
         </div>
       </div>
