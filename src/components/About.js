a9f20bcbfc1c51f0ebf6bfb398ab3c0913475d51
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-01-04 22:42:32 +0100

    adding about me page

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (  
+    <main className="about">
+      <div className="profile-picture"></div>
+      <div className="about-me"></div>
+    </main>
+  );
+}
+ 
+export default About;
