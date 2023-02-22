621aeb189567de578d1546ed1079208b82536581
harun <ibrahimovic.harun01@gmail.com>
2023-02-22 11:36:19 +0100

    installed firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtvOuJ1qHacVH0Xv1BBTJQl9VaIvb_Rtk",
+  authDomain: "eldar-portfolio.firebaseapp.com",
+  projectId: "eldar-portfolio",
+  storageBucket: "eldar-portfolio.appspot.com",
+  messagingSenderId: "166604757151",
+  appId: "1:166604757151:web:eb5875c33c4a99405beaaa"
+};
+
+const app = initializeApp(firebaseConfig);
