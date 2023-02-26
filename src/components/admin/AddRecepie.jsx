ff73550487137825caf72b2dd222e6b69ba33ee2
Harun29 <117188375+Harun29@users.noreply.github.com>
2023-02-26 13:48:04 +0100

    added uploading images

--- src/components/admin/AddRecepie.jsx
+++ src/components/admin/AddRecepie.jsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 import { db } from "../../config/firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { useNavigate } from "react-router";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "firebase/storage";
+import { storage } from "../../config/firebase";
+import { v4 } from "uuid";
 
 const AddRecepie = () => {
 
@@ -14,12 +21,41 @@ const AddRecepie = () => {
 
   const [recepie, setRecepie] = useState()
 
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imgURL, setImgURL] = useState('')
+
+  const uploadFile = async () => {
+    try {
+      if (imageUpload == null) return;
+      const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+      const snapshot = await uploadBytes(imageRef, imageUpload);
+      const url = await getDownloadURL(snapshot.ref);
+      setImgURL(url);
+      console.log(url);
+    } catch (err){
+      console.error('Error adding image: ', err);
+    }
+  };
+
   const addData = async (data) => {
     try {
+      console.log('here 2')
       const docRef = await addDoc(collection(db, 'recepies'), data);
       console.log('Document written with ID: ', docRef.id);
-    } catch (e) {
-      console.error('Error adding document: ', e);
+    } catch (err) {
+      console.error('Error adding document: ', err);
+    }
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+      setLoading(true);
+      await uploadFile();
+      await addData(recepie);
+      navigate('/')
+    } catch (err) {
+      setError('Failed to add recepie! Error: ', err)
     }
   }
 
@@ -27,20 +63,10 @@ const AddRecepie = () => {
     setRecepie({
       title: title,
       sdescription: shortDescription,
-      fdescription: fullDescription
+      fdescription: fullDescription,
+      imgurl: imgURL
     });
-  }, [title, shortDescription, fullDescription])
-
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-    try{
-      setLoading(true);
-      await addData(recepie);
-      navigate('/')
-    } catch (err) {
-      setError('Failed to add recepie! Error: ', err)
-    }
-  }
+  }, [title, shortDescription, fullDescription, imgURL])
 
   return (
     <form 
@@ -74,7 +100,13 @@ const AddRecepie = () => {
         onChange={(e) => setFullDescription(e.target.value)} 
         />
       </div>
-      {/* <div className="add-image"></div> */}
+      <div className="add-image">
+        <input 
+        type="file" 
+        onChange={(e) => {
+          setImageUpload(e.target.files[0]);
+        }}/>
+      </div>
       <button disabled={loading} type="submit" className="recepie-submit">Submit</button>
 
       {error ? 
